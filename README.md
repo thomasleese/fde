0861508eaa67367cd50813837a6740bf1c531f2a
Tom Leese <tom@tomleese.me.uk>
2013-12-07 11:07:19 +0000

    Add option to disable fullscreen mode, close #1

--- README.md
+++ README.md
@@ -16,5 +16,6 @@ Once built, you should be able to run it like this:
 
     $ ./build/fde
 
-The program runs in full-screen mode as it is designed for classroom use. You can however just Alt-Tab out of it and change the window properties from there.
+The program runs in full-screen mode as it is designed for classroom use. If you do not want this, you should run it like this:
 
+    $ ./build/fde --no-fullscreen
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
-#include "mainwindow.h"
+#include <iostream>
 #include <QApplication>
+
+#include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    bool fullscreenMode = true;
+    foreach (QString arg, a.arguments()) {
+        if (arg == "--help" || arg == "-h") {
+            std::cout << "Usage: " << a.arguments()[0].toStdString() << " [--help] [--no-fullscreen]" << std::endl;
+            return 0;
+        } else if (arg == "--no-fullscreen") {
+            fullscreenMode = false;
+        }
+    }
+
     MainWindow w;
-    w.showFullScreen();
-    
+    if (fullscreenMode) {
+        w.showFullScreen();
+    } else {
+        w.show();
+    }
+
     return a.exec();
 }
