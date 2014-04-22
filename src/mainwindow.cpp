27e4dd32085ed04344f90fe329dd3ce13a20bfcd
Tom Leese <tom@tomleese.me.uk>
2014-04-22 15:49:12 +0100

    Add d-pointer for MainWindow

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -8,28 +8,42 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+
+class MainWindowPrivate {
+public:
+    Component *alu, *cir;
+    Component *pc, *accumulator;
+    Component *cu, *mar, *mdr;
+
+    QString operand, code;
+    int tick;
+    char *currentTick;
+    bool continous;
+};
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent), d(new MainWindowPrivate), ui(new Ui::MainWindow) {
     ui->setupUi(this);
-    
-    mALU = new Component("Arithmetic Logic Unit",           0,      0,      0);
-    mCIR = new Component("Current Instruction Register",    110,    0,      50);
-    mPC = new Component("Program Counter",                  220,    0,      100);
-    mAccumulator = new Component("Accumulator",             0,      110,    150);
-    mCU = new Component("Control Unit",                     110,    110,    200);
-    mMAR = new Component("Memory Address Register",         220,    110,    250);
-    mMDR = new Component("Memory Data Register",            0,      220,    300);
-    
+
+    d->alu = new Component("Arithmetic Logic Unit",           0,      0,      0);
+    d->cir = new Component("Current Instruction Register",    110,    0,      50);
+    d->pc = new Component("Program Counter",                  220,    0,      100);
+    d->accumulator = new Component("Accumulator",             0,      110,    150);
+    d->cu = new Component("Control Unit",                     110,    110,    200);
+    d->mar = new Component("Memory Address Register",         220,    110,    250);
+    d->mdr = new Component("Memory Data Register",            0,      220,    300);
+
     QGraphicsScene *scene = new QGraphicsScene(this);
-    scene->addItem(mALU);
-    scene->addItem(mCIR);
-    scene->addItem(mPC);
-    scene->addItem(mAccumulator);
-    scene->addItem(mCU);
-    scene->addItem(mMAR);
-    scene->addItem(mMDR);
-    
+    scene->addItem(d->alu);
+    scene->addItem(d->cir);
+    scene->addItem(d->pc);
+    scene->addItem(d->accumulator);
+    scene->addItem(d->cu);
+    scene->addItem(d->mar);
+    scene->addItem(d->mdr);
+
     ui->graphicsView->setScene(scene);
-        
+
     on_actionStop_triggered();
 }
 
@@ -38,76 +52,76 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::makeActive(Component *component) {
-    mALU->setActive(false);
-    mCIR->setActive(false);
-    mPC->setActive(false);
-    mAccumulator->setActive(false);
-    mCU->setActive(false);
-    mMAR->setActive(false);
-    mMDR->setActive(false);
-    
+    d->alu->setActive(false);
+    d->cir->setActive(false);
+    d->pc->setActive(false);
+    d->accumulator->setActive(false);
+    d->cu->setActive(false);
+    d->mar->setActive(false);
+    d->mdr->setActive(false);
+
     if (component != 0) {
         component->setActive(true);
     }
 }
 
 void MainWindow::nextTick(const char *slot, bool force) {
-    mCurrentTick = (char *) slot;
-    
+    d->currentTick = (char *) slot;
+
     if (force) {
-        mTick--; // it gets ++ later
+        d->tick--; // it gets ++ later
         QTimer::singleShot(0, this, slot);
-    } else if (mContinous) {
+    } else if (d->continous) {
         int timeout = ui->sliderTimePerTick->value();
         QTimer::singleShot(timeout, this, slot);
     }
-    
-    mTick++;
-    ui->lblTick->setText(QString::number(mTick));
+
+    d->tick++;
+    ui->lblTick->setText(QString::number(d->tick));
 }
 
 void MainWindow::fetch1() {
     ui->lblStatus->setText("Fetching");
-    
-    makeActive(mPC);
-    
+
+    makeActive(d->pc);
+
     nextTick(SLOT(fetch2()));
 }
 
 void MainWindow::fetch2() {
-    makeActive(mMAR);
-    mMAR->setRawValue(mPC->getRawValue());
-    
+    makeActive(d->mar);
+    d->mar->setRawValue(d->pc->getRawValue());
+
     nextTick(SLOT(fetch3()));
 }
 
 void MainWindow::fetch3() {
-    makeActive(mMDR);
-    mMDR->setRawValue(ui->tblMemory->item(mMAR->getHexValue(), 0)->text());
-    
+    makeActive(d->mdr);
+    d->mdr->setRawValue(ui->tblMemory->item(d->mar->getHexValue(), 0)->text());
+
     nextTick(SLOT(fetch4()));
 }
 
 void MainWindow::fetch4() {
-    makeActive(mCIR);
-    mCIR->setRawValue(mMDR->getRawValue());
-    
+    makeActive(d->cir);
+    d->cir->setRawValue(d->mdr->getRawValue());
+
     nextTick(SLOT(decode1()));
 }
 
 void MainWindow::decode1() {
     ui->lblStatus->setText("Decoding");
-    
-    makeActive(mCU);
-    mCU->setRawValue(mCIR->getRawValue());
-    
-    QStringList instruction = mCU->getRawValue().split(QRegExp("\\s+"));
+
+    makeActive(d->cu);
+    d->cu->setRawValue(d->cir->getRawValue());
+
+    QStringList instruction = d->cu->getRawValue().split(QRegExp("\\s+"));
     QString op = instruction[0].toLower();
-    
+
     if (instruction.length() >= 2) {
-        mOperand = instruction[1];
+        d->operand = instruction[1];
     }
-    
+
     if (op == "load" || op == "lod") {
         nextTick(SLOT(executeLoad1()));
     } else if (op == "add") {
@@ -132,178 +146,178 @@ void MainWindow::decode1() {
 
 void MainWindow::executeLoad1() {
     ui->lblStatus->setText("Executing");
-    
-    makeActive(mMAR);
-    mMAR->setRawValue(mOperand);
-    
+
+    makeActive(d->mar);
+    d->mar->setRawValue(d->operand);
+
     nextTick(SLOT(executeLoad2()));
 }
 
 void MainWindow::executeLoad2() {
-    makeActive(mMDR);
-    mMDR->setRawValue(ui->tblMemory->item(mMAR->getHexValue(), 0)->text());
-    
+    makeActive(d->mdr);
+    d->mdr->setRawValue(ui->tblMemory->item(d->mar->getHexValue(), 0)->text());
+
     nextTick(SLOT(executeLoad3()));
 }
 
 void MainWindow::executeLoad3() {
-    makeActive(mAccumulator);
-    mAccumulator->setRawValue(mMDR->getRawValue());
-    
-    mPC->setHexValue(mPC->getHexValue() + 1);
+    makeActive(d->accumulator);
+    d->accumulator->setRawValue(d->mdr->getRawValue());
+
+    d->pc->setHexValue(d->pc->getHexValue() + 1);
     nextTick(SLOT(fetch1()));
 }
 
 void MainWindow::executeAdd1() {
     ui->lblStatus->setText("Executing");
-    
-    makeActive(mMAR);
-    mMAR->setRawValue(mOperand);
-    
+
+    makeActive(d->mar);
+    d->mar->setRawValue(d->operand);
+
     nextTick(SLOT(executeAdd2()));
 }
 
 void MainWindow::executeAdd2() {
-    makeActive(mMDR);
-    mMDR->setRawValue(ui->tblMemory->item(mMAR->getHexValue(), 0)->text());
-    
+    makeActive(d->mdr);
+    d->mdr->setRawValue(ui->tblMemory->item(d->mar->getHexValue(), 0)->text());
+
     nextTick(SLOT(executeAdd3()));
 }
 
 void MainWindow::executeAdd3() {
-    makeActive(mALU);
-    mALU->setDecValue(mAccumulator->getDecValue() + mMDR->getDecValue());
-    
+    makeActive(d->alu);
+    d->alu->setDecValue(d->accumulator->getDecValue() + d->mdr->getDecValue());
+
     nextTick(SLOT(executeAdd4()));
 }
 
 void MainWindow::executeAdd4() {
-    makeActive(mAccumulator);
-    mAccumulator->setRawValue(mALU->getRawValue());
-    mALU->setRawValue("NULL");
-    
-    mPC->setHexValue(mPC->getHexValue() + 1);
+    makeActive(d->accumulator);
+    d->accumulator->setRawValue(d->alu->getRawValue());
+    d->alu->setRawValue("NULL");
+
+    d->pc->setHexValue(d->pc->getHexValue() + 1);
     nextTick(SLOT(fetch1()));
 }
 
 void MainWindow::executeSub1() {
     ui->lblStatus->setText("Executing");
-    
-    makeActive(mMAR);
-    mMAR->setRawValue(mOperand);
-    
+
+    makeActive(d->mar);
+    d->mar->setRawValue(d->operand);
+
     nextTick(SLOT(executeSub2()));
 }
 
 void MainWindow::executeSub2() {
-    makeActive(mMDR);
-    mMDR->setRawValue(ui->tblMemory->item(mMAR->getHexValue(), 0)->text());
-    
+    makeActive(d->mdr);
+    d->mdr->setRawValue(ui->tblMemory->item(d->mar->getHexValue(), 0)->text());
+
     nextTick(SLOT(executeSub3()));
 }
 
 void MainWindow::executeSub3() {
-    makeActive(mALU);
-    mALU->setDecValue(mAccumulator->getDecValue() - mMDR->getDecValue());
-    
+    makeActive(d->alu);
+    d->alu->setDecValue(d->accumulator->getDecValue() - d->mdr->getDecValue());
+
     nextTick(SLOT(executeSub4()));
 }
 
 void MainWindow::executeSub4() {
-    makeActive(mAccumulator);
-    mAccumulator->setRawValue(mALU->getRawValue());
-    mALU->setRawValue("NULL");
-    
-    mPC->setHexValue(mPC->getHexValue() + 1);
+    makeActive(d->accumulator);
+    d->accumulator->setRawValue(d->alu->getRawValue());
+    d->alu->setRawValue("NULL");
+
+    d->pc->setHexValue(d->pc->getHexValue() + 1);
     nextTick(SLOT(fetch1()));
 }
 
 void MainWindow::executeBrz1() {
     ui->lblStatus->setText("Executing");
-    
-    makeActive(mMAR);
-    mMAR->setRawValue(mOperand);
-    
+
+    makeActive(d->mar);
+    d->mar->setRawValue(d->operand);
+
     nextTick(SLOT(executeBrz2()));
 }
 
 void MainWindow::executeBrz2() {
-    makeActive(mAccumulator);
-    if (mAccumulator->getDecValue() == 0) {
-        mPC->setHexValue(mMAR->getHexValue());
+    makeActive(d->accumulator);
+    if (d->accumulator->getDecValue() == 0) {
+        d->pc->setHexValue(d->mar->getHexValue());
     } else {
-        mPC->setHexValue(mPC->getHexValue() + 1);
+        d->pc->setHexValue(d->pc->getHexValue() + 1);
     }
-    
+
     nextTick(SLOT(fetch1()));
 }
 
 void MainWindow::executeBrn1() {
     ui->lblStatus->setText("Executing");
-    
-    makeActive(mMAR);
-    mMAR->setRawValue(mOperand);
-    
+
+    makeActive(d->mar);
+    d->mar->setRawValue(d->operand);
+
     nextTick(SLOT(executeBrn2()));
 }
 
 void MainWindow::executeBrn2() {
-    makeActive(mAccumulator);
-    if (mAccumulator->getDecValue() < 0) {
-        mPC->setHexValue(mMAR->getHexValue());
+    makeActive(d->accumulator);
+    if (d->accumulator->getDecValue() < 0) {
+        d->pc->setHexValue(d->mar->getHexValue());
     } else {
-        mPC->setHexValue(mPC->getHexValue() + 1);
+        d->pc->setHexValue(d->pc->getHexValue() + 1);
     }
-    
+
     nextTick(SLOT(fetch1()));
 }
 
 void MainWindow::executeStore1() {
     ui->lblStatus->setText("Executing");
-    
-    makeActive(mMAR);
-    mMAR->setRawValue(mOperand);
-    
+
+    makeActive(d->mar);
+    d->mar->setRawValue(d->operand);
+
     nextTick(SLOT(executeStore2()));
 }
 
 void MainWindow::executeStore2() {
-    makeActive(mAccumulator);
-    
+    makeActive(d->accumulator);
+
     nextTick(SLOT(executeStore3()));
 }
 
 void MainWindow::executeStore3() {
-    makeActive(mAccumulator);
-    
-    int sum = mAccumulator->getDecValue();
-    int row = mMAR->getHexValue();
+    makeActive(d->accumulator);
+
+    int sum = d->accumulator->getDecValue();
+    int row = d->mar->getHexValue();
     ui->tblMemory->setItem(row, 0, new QTableWidgetItem(QString::number(sum)));
-        
-    mPC->setHexValue(mPC->getHexValue() + 1);
+
+    d->pc->setHexValue(d->pc->getHexValue() + 1);
     nextTick(SLOT(fetch1()));
 }
 
 void MainWindow::executeJump1() {
     ui->lblStatus->setText("Executing");
-    
-    makeActive(mMAR);
-    mMAR->setRawValue(mOperand);
-    
-    mPC->setHexValue(mMAR->getHexValue());
+
+    makeActive(d->mar);
+    d->mar->setRawValue(d->operand);
+
+    d->pc->setHexValue(d->mar->getHexValue());
     nextTick(SLOT(fetch1()));
 }
 
 void MainWindow::executeStop1() {
     ui->lblStatus->setText("Stopped");
-    
+
     on_actionStop_triggered();
 }
 
 void MainWindow::on_actionLoadProgram_triggered() {
-    LoadProgramDialogue d(this);
-    if (d.exec() == QDialog::Accepted) {
-        mCode = d.getProgramCode();
+    LoadProgramDialogue dialogue(this);
+    if (dialogue.exec() == QDialog::Accepted) {
+        d->code = dialogue.getProgramCode();
         on_actionReloadCode_triggered();
         on_actionStop_triggered();
     }
@@ -311,33 +325,33 @@ void MainWindow::on_actionLoadProgram_triggered() {
 
 void MainWindow::on_actionStop_triggered() {
     ui->lblStatus->setText("Computer Reset");
-    
-    mALU->setRawValue("NULL");
-    mCIR->setRawValue("NULL");
-    mPC->setRawValue("0x00");
-    mAccumulator->setRawValue("NULL");
-    mCU->setRawValue("NULL");
-    mMAR->setRawValue("NULL");
-    mMDR->setRawValue("NULL");
-    
+
+    d->alu->setRawValue("NULL");
+    d->cir->setRawValue("NULL");
+    d->pc->setRawValue("0x00");
+    d->accumulator->setRawValue("NULL");
+    d->cu->setRawValue("NULL");
+    d->mar->setRawValue("NULL");
+    d->mdr->setRawValue("NULL");
+
     makeActive(0);
-    
+
     ui->actionPlay->setEnabled(true);
     ui->actionStop->setEnabled(false);
     ui->actionReloadCode->setEnabled(false);
     ui->actionStep->setEnabled(true);
     ui->actionPause->setEnabled(false);
-    
-    mTick = 0;
-    
-    mCurrentTick = (char *) SLOT(fetch1());
-    mContinous = false;
+
+    d->tick = 0;
+
+    d->currentTick = (char *) SLOT(fetch1());
+    d->continous = false;
 }
 
 void MainWindow::on_actionPlay_triggered() {
-    mContinous = true;
-    nextTick(mCurrentTick);
-    
+    d->continous = true;
+    nextTick(d->currentTick);
+
     ui->actionPlay->setEnabled(false);
     ui->actionStop->setEnabled(true);
     ui->actionReloadCode->setEnabled(true);
@@ -346,9 +360,9 @@ void MainWindow::on_actionPlay_triggered() {
 }
 
 void MainWindow::on_actionStep_triggered() {
-    mContinous = false;
-    nextTick(mCurrentTick, true);
-    
+    d->continous = false;
+    nextTick(d->currentTick, true);
+
     ui->actionPlay->setEnabled(true);
     ui->actionStop->setEnabled(true);
     ui->actionReloadCode->setEnabled(true);
@@ -357,8 +371,8 @@ void MainWindow::on_actionStep_triggered() {
 }
 
 void MainWindow::on_actionPause_triggered() {
-    mContinous = false;
-    
+    d->continous = false;
+
     ui->actionPlay->setEnabled(true);
     ui->actionStop->setEnabled(true);
     ui->actionReloadCode->setEnabled(true);
@@ -377,12 +391,12 @@ void MainWindow::on_actionInstructions_triggered() {
 
 void MainWindow::on_actionReloadCode_triggered() {
     ui->tblMemory->clearContents();
-    QStringList lines = mCode.split("\n");
+    QStringList lines = d->code.split("\n");
     for (int i = 0; i < lines.length(); i++) {
         QString line = lines[i];
         QTableWidgetItem *item = new QTableWidgetItem(line);
         ui->tblMemory->setItem(i, 0, item);
     }
-    
+
     on_actionStop_triggered();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -10,75 +10,66 @@ namespace Ui {
 }
 
 class MainWindow : public QMainWindow {
-    
     Q_OBJECT
-    
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-        
+
     void makeActive(Component *component);
     void nextTick(const char *slot, bool force = false);
-    
+
 private slots:
     void fetch1();
     void fetch2();
     void fetch3();
     void fetch4();
-    
+
     void decode1();
-    
+
     void executeLoad1();
     void executeLoad2();
     void executeLoad3();
-    
+
     void executeAdd1();
     void executeAdd2();
     void executeAdd3();
     void executeAdd4();
-    
+
     void executeSub1();
     void executeSub2();
     void executeSub3();
     void executeSub4();
-    
+
     void executeBrz1();
     void executeBrz2();
-    
+
     void executeBrn1();
     void executeBrn2();
-    
+
     void executeStore1();
     void executeStore2();
     void executeStore3();
-    
+
     void executeJump1();
-    
+
     void executeStop1();
-    
+
     void on_actionLoadProgram_triggered();
     void on_actionStop_triggered();
     void on_actionPlay_triggered();
-    
+
     void on_actionStep_triggered();
-    
+
     void on_actionPause_triggered();
-    
+
     void on_actionInstructions_triggered();
-    
+
     void on_actionReloadCode_triggered();
-    
+
 private:
     Ui::MainWindow *ui;
-    
-    Component *mALU, *mCIR;
-    Component *mPC, *mAccumulator;
-    Component *mCU, *mMAR, *mMDR;
-    
-    QString mOperand, mCode;
-    int mTick;
-    char *mCurrentTick;
-    bool mContinous;
+    class MainWindowPrivate *d;
 };
 
 #endif // MAINWINDOW_H
